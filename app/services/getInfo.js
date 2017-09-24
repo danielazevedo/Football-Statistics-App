9746147ae8ed925e27313f32b30e9fb7f1856ac2
danielazevedo <daniel96.azevedo@gmail.com>
2017-09-24 17:50:39 +0100

    indent code

--- app/services/getInfo.js
+++ app/services/getInfo.js
@@ -1,84 +1,84 @@
 
 
 app.factory('getInfo', ['$http', 'sharedProperties', function($http,sharedProperties) { 
-  return{
-			make_request:  function(sharedProperties){
-				var request = $http.get('http://localhost:4567/getNextGames?league='+sharedProperties.getLeagueName().toString()) 
-	            .success(function(data) {
-	              return data; 
-	            }) 
-	            .error(function(err) { 
-	              console.log(err)
-	              return err; 
-	            }); 
-        		return request
-        	}
-        }
+	return{
+		make_request:  function(sharedProperties){
+			var request = $http.get('http://localhost:4567/getNextGames?league='+sharedProperties.getLeagueName().toString()) 
+				.success(function(data) {
+					return data; 
+				}) 
+			.error(function(err) { 
+				console.log(err)
+					return err; 
+			}); 
+			return request
+		}
+	}
 }]);
 
 
 
 app.factory('getInfoTotal', ['$http', function($http) { 
-  return {
-  			make_request: function(team){
-	  			var request = $http.get('http://localhost:4567/getInfoTotal?team='+team) 
+	return {
+		make_request: function(team){
+			var request = $http.get('http://localhost:4567/getInfoTotal?team='+team) 
 				.success(function(data) { 
-	              return data; 
-	            }) 
-	            .error(function(err) { 
-	              console.log(err)
-	              return err; 
-	            }); 
-	          	return request
-	        }
-    	}
+					return data; 
+				}) 
+			.error(function(err) { 
+				console.log(err)
+					return err; 
+			}); 
+			return request
+		}
+	}
 }]);
 
 
 app.factory('getInfoHome', ['$http', function($http) { 
-  return {
-  			make_request: function(team){
-	  			var request = $http.get('http://localhost:4567/getInfoHome?team='+team) 
+	return {
+		make_request: function(team){
+			var request = $http.get('http://localhost:4567/getInfoHome?team='+team) 
 				.success(function(data) { 
-	              return data; 
-	            }) 
-	            .error(function(err) { 
-	              console.log(err)
-	              return err; 
-	            }); 
-	          	return request
-	        }
-    	}
+					return data; 
+				}) 
+			.error(function(err) { 
+				console.log(err)
+					return err; 
+			}); 
+			return request
+		}
+	}
 }]);
 
 app.factory('getInfoAway', ['$http', function($http) { 
-  return {
-  			make_request: function(team){
-	  			var request = $http.get('http://localhost:4567/getInfoAway?team='+team) 
+	return {
+		make_request: function(team){
+			var request = $http.get('http://localhost:4567/getInfoAway?team='+team) 
 				.success(function(data) { 
-	              return data; 
-	            }) 
-	            .error(function(err) { 
-	              console.log(err)
-	              return err; 
-	            }); 
-	          	return request
-	        }
-    	}
+					return data; 
+				}) 
+			.error(function(err) { 
+				console.log(err)
+					return err; 
+			}); 
+			return request
+		}
+	}
 }]);
 
 app.factory('getPrevGames', ['$http', 'sharedProperties', function($http,sharedProperties) { 
-  return {
-  			make_request: function(sharedProperties){
-	  			var request = $http.get('http://localhost:4567/getPrevGames?team1='+sharedProperties.getHomeTeam()+'+&team2='+sharedProperties.getAwayTeam()) 
+	return {
+		make_request: function(sharedProperties){
+			var request = $http.get('http://localhost:4567/getPrevGames?team1='+sharedProperties.getHomeTeam()+'+&team2='+sharedProperties.getAwayTeam()) 
 				.success(function(data) { 
-	              return data; 
-	            }) 
-	            .error(function(err) { 
-	              console.log(err)
-	              return err; 
-	            }); 
-	          	return request
-	        }
-    	}
+					return data; 
+				}) 
+			.error(function(err) { 
+				console.log(err)
+					return err; 
+			}); 
+			return request
+		}
+	}
 }]);
